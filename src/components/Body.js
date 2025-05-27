8049b2ac8bd840382d9dfa17215c86d76ad27fda
Harsh <harsh.malik@highwirepress.com>
2025-05-27 14:38:48 +0530

    remove the fetch as it causing the cors issue

--- src/components/Body.js
+++ src/components/Body.js
@@ -17,13 +17,11 @@ const Body = () => {
   }, []);
 
   const fetchData = async () => {
-    const swiggyURL = encodeURIComponent(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-    );
+    // const swiggyURL = encodeURIComponent(
+    //   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    // );
 
-    const result = await fetch(
-      `https://api.allorigins.win/raw?url=${swiggyURL}`
-    );
+    const result = await fetch("/data.json");
     // const swiggyApiUrl =
     //   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
     // const proxyUrl = `https://proxy.corsfix.com/?${encodeURIComponent(
