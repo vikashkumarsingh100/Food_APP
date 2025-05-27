44a63a7e80bc16c42eeb047218370dc4f6214014
Harsh <harsh.malik@highwirepress.com>
2025-05-27 15:01:08 +0530

    Add local Swiggy data

--- src/components/Body.js
+++ src/components/Body.js
@@ -21,7 +21,7 @@ const Body = () => {
     //   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     // );
 
-    const result = await fetch("/data.json");
+    const result = await fetch("./data.json");
     // const swiggyApiUrl =
     //   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
     // const proxyUrl = `https://proxy.corsfix.com/?${encodeURIComponent(
