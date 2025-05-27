08c311deb3e4c451974ca9c83433fa747846ac6c
Harsh <harsh.malik@highwirepress.com>
2025-05-27 12:06:58 +0530

    Updates

---
+++ Grocery.d97b509b.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire28eb;(0,e.register)("loR3J",function(r,o){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>a,set:void 0,enumerable:!0,configurable:!0});var t=e("ayMG0"),a=()=>(0,t.jsxs)("h1",{children:["","Our Grocery Store ,and it have a lot of child component inside the web page."]})});
+//# sourceMappingURL=Grocery.d97b509b.js.map
